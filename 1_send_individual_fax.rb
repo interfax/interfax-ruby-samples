dd77a6420f5730590bba364708f97df9d58353af
eyalnevo <eyal@interfax.net>
2017-03-06 12:05:01 -0600

    Update 1_send_individual_fax.rb

--- 1_send_individual_fax.rb
+++ 1_send_individual_fax.rb
@@ -1,7 +1,3 @@
-# This sample lives at https://github.com/interfax/interfax-ruby-samples/edit/master/1_send_individual_fax.rb
-# Find more Ruby code samples at https://github.com/interfax/interfax-ruby/samples
-# Find a discussion of this sample at https://www.interfax.net/en/dev/ruby/send-individual-fax
-
 # send a single fax
 fax = interfax.deliver(
   # a valid fax number
